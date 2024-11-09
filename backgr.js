9c00ace6d394be46936df9ae6019d3b5590dd0b0
noviyashik50285 <mynewdisk50285@gmail.com>
2024-11-09 18:24:03 +0300

    secnew commit

---
+++ backgr.js
@@ -0,0 +1,50 @@
+const button1 = document.getElementById('butn1');
+button1.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria0.jpg)';
+});
+const button2 = document.getElementById('butn2');
+button2.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria1.jpg)';
+});
+const button3 = document.getElementById('butn3');
+button3.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria2.jpg)';
+});
+const button4 = document.getElementById('butn4');
+button4.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria3.jpg)';
+});
+const button5 = document.getElementById('butn5');
+button5.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria4.jpg)';
+});
+const button6 = document.getElementById('butn6');
+button6.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria5.jpg)';
+});
+const button7 = document.getElementById('butn7');
+button7.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria6.jpg)';
+});
+const button8 = document.getElementById('butn8');
+button8.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria7.jpg)';
+});
+const button9 = document.getElementById('butn9');
+button9.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria8.jpg)';
+});
+const button10 = document.getElementById('butn10');
+button10.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/victoria9.jpg)';
+});
